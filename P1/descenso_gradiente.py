80da17d5054bd7dffcd34618388fd866f82b4e41
Gilberto López <gilisaac@ciencias.unam.mx>
2017-08-24 17:49:42 -0500

    Ajuste de la constante de aprendizaje.

--- P1/descenso_gradiente.py
+++ P1/descenso_gradiente.py
@@ -1,13 +1,13 @@
 import numpy as np
 import random
 
-### Descenso por el gradiente para mínimos cuadrados.
-
-# w es el vector con los pesos w = [w0 ... wn]^t
+# w es el vector con los pesos w = [w0 ... wn]^t.
 # X son los datos: X = [X^1 ... X^m]^t donde X^i es un ejemplar
-# de dimensión n (aumentado), ie, X^i = [1 X^i_1 ... X^i_n].
+# de dimensión n (aumentado), ie, X^i = [1 X^i_1 ... X^i_n]^t.
 # y es el vector con los resultados y = [y^1 ... y^m]^t donde
-# y^i es el resultado del ejemplar X^i
+# y^i es el resultado del ejemplar X^i.
+
+### Descenso por el gradiente para mínimos cuadrados.
 
 def h_ls (w,X):
   return np.dot(X,w)
@@ -24,8 +24,7 @@ def GJ_ls (w,X,y,h):
   s = h(w,X)-y
   return np.dot(np.transpose (X),s)
 
-def descenso_gradiente (J,GJ,X,y,w,h,e=0.1,alpha=0.2,it=1000):
-  q = len(w)
+def descenso_gradiente (J,GJ,X,y,w,h,e=0.1,alpha=0.02,it=1000):
   P = np.copy(w)
   k = 0
   while J(P,X,y,h) >= e and k < it:
@@ -36,5 +35,6 @@ def descenso_gradiente (J,GJ,X,y,w,h,e=0.1,alpha=0.2,it=1000):
 if __name__ == '__main__':
   X = np.matrix([[1,1],[1,2],[1,3],[1,4]])
   y = np.matrix([[2],[3],[5],[7]])
-  w = np.matrix([[random.uniform(-0.5,0.5)],[random.uniform(-0.5,0.5)]])
-  descenso_gradiente(J_ls,GJ_ls,X,y,w,h_ls)
+  w = np.matrix([[1],[2]])
+  w_f = descenso_gradiente(J_ls,GJ_ls,X,y,w,h_ls)
+  print(w_f)
