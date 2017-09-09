106a3b72c65a49aeb54e0b3193e58f966d137782
Gilberto López <gilisaac@ciencias.unam.mx>
2017-09-08 21:56:10 -0500

    Método de regresión logística, correcciones pendientes. Overflow en aplicación de np.exp.

--- P1/descenso_gradiente.py
+++ P1/descenso_gradiente.py
@@ -24,10 +24,39 @@ def GJ_ls (w,X,y,h):
   s = h(w,X)-y
   return np.dot(np.transpose (X),s)
 
+### Descenso por el gradiente para regresión logística
+# w es el vector con los pesos w = [w0 ... wn c]^t.
+
+def J_lr (w,X,y,h):
+  C_ = 1 # parámetro
+  c = w[-1,0]
+  m = len(X)
+  s = 0
+  for i in range(m):
+    s += np.log(1+np.exp(-y[i,0]*(c+np.dot(X[i,:],w[:-1,:]))))
+  return np.dot(np.transpose(w),w)/2 + C_*s
+
+def GJ_lr (w,X,y,h):
+  C_ = 1 # parámetro
+  c = w[-1,0]
+  p = np.copy(w)
+  n = len(w)
+  m = len(X)
+  q = np.matrix([0]*m).transpose()
+  for i in range(m):
+    q[i,0] = y[i,0]/(1+np.exp(y[i,0]*(c+np.dot(X[i,:],w[:-1,:]))))
+  p[-1,0] = C_*sum(q)
+  for j in range(n-1):
+    p[j,0] = w[j,0] + C_*np.dot(np.transpose(X[:,j]),q)
+  return p
+
 def descenso_gradiente (J,GJ,X,y,w,h,e=0.1,alpha=0.02,it=1000):
   P = np.copy(w)
   k = 0
   while J(P,X,y,h) >= e and k < it:
+    print("---------------")
+    print(P)
+    print(J(P,X,y,h))
     k += 1
     P = P - alpha*GJ(P,X,y,h)
   return P
@@ -35,6 +64,9 @@ def descenso_gradiente (J,GJ,X,y,w,h,e=0.1,alpha=0.02,it=1000):
 if __name__ == '__main__':
   X = np.matrix([[1,1],[1,2],[1,3],[1,4]])
   y = np.matrix([[2],[3],[5],[7]])
-  w = np.matrix([[1],[2]])
-  w_f = descenso_gradiente(J_ls,GJ_ls,X,y,w,h_ls)
-  print(w_f)
+  #w = np.matrix([[0],[0]])
+  #w_f = descenso_gradiente(J_ls,GJ_ls,X,y,w,h_ls)
+  #print(w_f)
+  w =  np.matrix([[0],[0],[0]])
+  w_g = descenso_gradiente(J_lr,GJ_lr,X,y,w,None)
+  print(w_g)
