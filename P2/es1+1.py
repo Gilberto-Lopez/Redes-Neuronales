b5c6a71844f620b5c30dc12855f6427779fa7f04
Gilberto López <gilisaac@ciencias.unam.mx>
2017-10-05 21:48:58 -0500

    Descripción del código.

--- P2/es1+1.py
+++ P2/es1+1.py
@@ -13,24 +13,39 @@ def funcion(x):
     return ((x-10)**4) - (50 * (x-10)**2) + (8*x) - 100
 
 if __name__ == '__main__':
-  iteraciones = 200
+  # Número máximo de iteraciones
+  iteraciones = 2000
+  # Rango de interés
   x1 = 2.0
   x2 = 18.0
+  # 
   c = 0.817
+  # El número de variables para f es 1
   f = funcion
 
+  # Punto inicial
   X = randint(x1,x2)
+  # f(X), su fitness
   p = f(X)
+  # Soluciones encontradas y su valor
   xs = [X]
   ys = [p]
+  # Mutaciones exitosas (mejor resultado que la solución actual)
   S = 0
+  # Mutaciones no exitosas (no es mejor que la solución actual)
   F = 0
+  # Relación mutaciones exitosas sobre mutaciones totales
   ps = 0
-  sig = 0.1
+  # Sigma la desviación estándar de la distribución normal
+  # Para sigma = 1 tiende a quedarse atorado en mínimos locales.
+  sig = 0.5
   for _ in range(iteraciones):
+    # Generamos una nueva solución a partir de una mutación sobre la actual
     X_ = X + sig*np.random.normal()
+    # Calculamos su fitness
     p_ = f(X_)
     if p_ < p:
+      # La nueva solución es mejor
       X = X_
       p = p_
       xs.append(X)
@@ -38,8 +53,9 @@ if __name__ == '__main__':
       S += 1
     else:
       F += 1
-    #xs.append(X)
+    # Actualizamos ps
     ps = S / float(S+F)
+    # Ajustamos sigma
     if ps < 1.0/5:
       sig = sig*c
     elif ps > 1.0/5:
